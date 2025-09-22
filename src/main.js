87c74bc683e83135505070bfa18adf62981cfc18
Ssask <122962997+ssask-script@users.noreply.github.com>
2025-09-22 09:45:24 +0545

    fix bug

--- src/main.js
+++ src/main.js
@@ -2,14 +2,13 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { createMetaManager } from 'vue-meta'
 import App from './App.vue'
 import router from './router'
 import { createHead } from '@vueuse/head'
 
 const app = createApp(App)
 const head = createHead()
-app.use(createMetaManager())
+
 app.use(createPinia())
 app.use(router)
 app.use(head)
